d173d1136b4bd4c55f183d2a6545b0febf6c67c2
syoukeles <syoukeles@gmail.com>
2022-10-04 20:19:10 -0400

    Added basic walk animation

---
+++ PenguinNurse/Assets/Scripts/WalkAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAnimation : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis ("Horizontal") > 0 || Input.GetAxis ("Horizontal") < 0){
+            anim.enabled = true;
+        } else {
+            anim.enabled = false;
+        }
+    }
+}
